a965913e9af2f22ba18d239d790225885b55056a
mahamudsourov <sourovm554@gmail.com>
2025-04-07 00:35:08 +0600

    updated_homescreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,22 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
-import 'screens/signup_screen.dart';
-import 'screens/home_screen.dart';
 
 void main() {
-  runApp(LostAndFoundApp());
+  runApp(const MyApp());
 }
 
-class LostAndFoundApp extends StatelessWidget {
-  const LostAndFoundApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Lost & Found',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/signup': (context) => SignupScreen(),
-        '/home': (context) => HomeScreen(), 
-      },
+      theme: ThemeData.light(), // Light theme
+      darkTheme: ThemeData.dark(), // Dark theme
+      themeMode: ThemeMode.system, // The app uses the system default theme
+      home: LoginScreen(), // Login screen will be the starting screen
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lost_and_found/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const LostAndFoundApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
