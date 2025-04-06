f1d26a9798d9e461369c3503ac1a7e4e2523662e
mahamudsourov <sourovm554@gmail.com>
2025-04-07 00:38:09 +0600

    added_item_card

---
+++ lib/screens/item_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ItemCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+  final bool showClaimButton;
+
+  ItemCard({
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.showClaimButton,
+  });
+
+  void claimItem(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Claim request sent for $title!')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      elevation: 3,
+      child: ListTile(
+        leading: Image.network(imageUrl, width: 50, height: 50, fit: BoxFit.cover),
+        title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(description),
+        trailing: showClaimButton
+            ? ElevatedButton(
+                onPressed: () => claimItem(context),
+                child: Text('Claim'),
+              )
+            : null,
+      ),
+    );
+  }
+}
