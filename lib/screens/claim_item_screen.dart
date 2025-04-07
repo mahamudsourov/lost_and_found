9d89dfc8a2ff91e6eed453cacde8b45a73d5a395
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2025-04-07 19:17:05 +0600

    claim_item_screen

---
+++ lib/screens/claim_item_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ClaimItemScreen extends StatelessWidget {
+  const ClaimItemScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Claim an Item')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Claim an item by providing proof'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
